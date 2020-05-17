5587f304f95fc5df90ca090bf5d9390235d46fe9
Nikita Luzan <luzanikita@gmail.com>
2020-05-17 13:46:17 +0300

    Localization base added.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./HomePage.module.css";
+import LocalizedStrings from 'react-localization';
+ 
+let strings = new LocalizedStrings({
+ en:{
+   welcome: "WELCOME TO FISH FARM"
+ },
+ ua: {
+   welcome: "ЛАСКАВО ПРОСИМО ДО FISH FARM"
+ }
+});
 
 const HomePage = () => {
+  strings.setLanguage('en');
+
   return (
     <div className={styles["home-page"]}>
-      <div>WELCOME TO FISH FARM</div>
+      <div>{strings.welcome}</div>
     </div>
   );
 };
